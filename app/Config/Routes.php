59105b94dc0c9cb783a19b62bf68de6aea872f7d
Gabi <40302142+kuhg@users.noreply.github.com>
2025-05-09 18:20:33 -0300

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,3 +16,11 @@ $routes->get('cargar_amo', 'Amos::cargar');
 $routes->post('guardar', 'Amos::guardar');
 $routes->get('borrar/(:num)', 'Amos::borrar/$1'); $routes->get('editar/(:num)', 'Amos::editar/$1');
 $routes->post('actualizar', 'Amos::actualizar');
+
+$routes->get('CargarMascota','CargarMascota::index');
+$routes->post('form/cargarMascota','CargarMascota::cargarMascota');
+$routes->get('VerMascotas','VerMascotas::index');
+
+$routes->get('ModificarMascota/(:num)','ModificarMascota::index/$1');
+$routes->post('form/modificarMascota','ModificarMascota::modificar');
+$routes->post('form/adoptarMascota', 'VerMascotas::adoparMascota');
